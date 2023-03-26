62d7125414d1e62e38a6e9f151b28f2b9f44d608
pengjixiang007 <128967855+pengjixiang007@users.noreply.github.com>
2023-03-26 22:01:31 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def get_weather(region):
     ak = config["weather_key"]
     region_url = "https://api.map.baidu.com/weather/v1/?district_id=350703&data_type=all&ak=uZA8XlUaw1xwUrBsRzxfjwjPBUwGFVHB"
     response = get(region_url, headers=headers).json()
-        # 获取地区的district_id
+    # 获取地区的district_id
     city = response["city"]["name"]
     weather_url = "https://api.map.baidu.com/weather/v1/?district_id=350703&data_type=all&ak=uZA8XlUaw1xwUrBsRzxfjwjPBUwGFVHB"
     response = get(weather_url, headers=headers).json()
@@ -49,7 +49,7 @@ def get_weather(region):
     temp = response["now"]["temp"] + u"\N{DEGREE SIGN}" + "C"
     # 风向
     wind_dir = response["now"]["windDir"]
-    return weather, temp, wind_dir
+    return city,name,weather,temp,wind_dir
  
  
 def get_birthday(birthday, year, today):
